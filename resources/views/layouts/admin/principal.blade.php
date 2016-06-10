bea3267fc08eade6f173a501a5d7345d5fcd7014
mdxshana <deiby.tovar@ceindetec.org.co>
2016-06-10 08:54:51 -0500

    prefecto de cargando a paginas

--- resources/views/layouts/admin/principal.blade.php
+++ resources/views/layouts/admin/principal.blade.php
@@ -3,13 +3,13 @@
 
 <head>
 
-    <meta charset="utf-8"> 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name='csrf-param' content='authenticity_token'>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="utf-8"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name='csrf-param' content='authenticity_token'>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>Control RFID! | </title>
 
@@ -35,24 +35,36 @@
   
   {!!Html::style('vendors/datatable/css/jquery.dataTables.css')!!}
 
- 
+  {!!Html::style('css/general.css')!!}
 
   @yield('css')
 
 </head>
 
 <body class="nav-md">
+
+  <div class="animacarga">
+    
+    <div class="cssload-thecube">
+      <div class="cssload-cube cssload-c1"></div>
+      <div class="cssload-cube cssload-c2"></div>
+      <div class="cssload-cube cssload-c4"></div>
+      <div class="cssload-cube cssload-c3"></div>
+    </div>
+
+  </div>
+
   <div class="container body">
     <div class="main_container">
       <div class="col-md-3 left_col">
         <div class="left_col scroll-view">
           <div class="navbar nav_title" style="border: 0;">
             <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Control RFID!</span></a>
-        </div>
+          </div>
 
-        <div class="clearfix"></div>
+          <div class="clearfix"></div>
 
-        <!-- menu profile quick info -->
+          <!-- menu profile quick info -->
           <!-- <div class="profile">
             <div class="profile_pic">
               <img src="images/img.jpg" alt="..." class="img-circle profile_img">
@@ -61,56 +73,56 @@
               <span>Welcome,</span>
               <h2>John Doe</h2>
             </div>
-        </div>  Esta seria usada en caso que este manejando usuarios con avatar   --> 
-        <!-- /menu profile quick info -->
+          </div>  Esta seria usada en caso que este manejando usuarios con avatar   --> 
+          <!-- /menu profile quick info -->
 
-        <br />
+          <br />
 
-        <!-- sidebar menu -->
-        @include('layouts.admin.menusliderbar')
-        <!-- /sidebar menu -->
+          <!-- sidebar menu -->
+          @include('layouts.admin.menusliderbar')
+          <!-- /sidebar menu -->
 
-        <!-- /menu footer buttons -->
-        <!--@include('layouts.admin.menufootherslider')-->
-        <!-- /menu footer buttons -->
-    </div>
-</div>
+          <!-- /menu footer buttons -->
+          <!--@include('layouts.admin.menufootherslider')-->
+          <!-- /menu footer buttons -->
+        </div>
+      </div>
 
-<!-- top navigation -->
+      <!-- top navigation -->
 
-@include('layouts.admin.menutop')
+      @include('layouts.admin.menutop')
 
-<!-- /top navigation -->
+      <!-- /top navigation -->
 
-<!-- page content -->
-<div class="right_col" role="main">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-       @yield('content')
-   </div>
+      <!-- page content -->
+      <div class="right_col" role="main">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+         @yield('content')
+       </div>
 
 
-</div>
-<!-- /page content -->
+     </div>
+     <!-- /page content -->
 
-<!-- footer content -->
-<footer>
-    <div class="pull-right">
-      Ceindetec llanos - 2016
+     <!-- footer content -->
+     <footer>
+      <div class="pull-right">
+        Ceindetec llanos - 2016
+      </div>
+      <div class="clearfix"></div>
+    </footer>
+    <!-- /footer content -->
   </div>
-  <div class="clearfix"></div>
-</footer>
-<!-- /footer content -->
-</div>
 </div>
 
 
 <!-- Modal Bootstrap-->
-    <div id='modalBs' class='modal fade bs-example-modal-lg'>
-        <div class="modal-dialog">
-            <div class="modal-content">
-            </div>
-        </div>
+<div id='modalBs' class='modal fade bs-example-modal-lg'>
+  <div class="modal-dialog">
+    <div class="modal-content">
     </div>
+  </div>
+</div>
 
 <!-- jQuery -->
 {!!Html::script('vendors/jquery/dist/jquery.min.js')!!}
